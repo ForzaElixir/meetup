4b92b7a4096c6846aeba63bbef2f76351761c4e3
Fredrik Möllerstrand <fredrik@footballaddicts.com>
2016-02-11 16:27:03 +0100

    posts!

--- dumblr/lib/dumblr/web.ex
+++ dumblr/lib/dumblr/web.ex
@@ -6,10 +6,20 @@ defmodule Dumblr.Web do
   plug :dispatch
 
   get "/" do
-    template = File.read!(Application.app_dir(:dumblr, "priv/web/index.html.eex"))
-    content = EEx.eval_string(template, [title: "Dumblr Blog"])
+    posts = [
+      %{title: "Good news", content: "I have good news."},
+      %{title: "Bad news", content: "I have some bad news."},
+    ]
+    content = render("index.html.eex", blog_title: "Dumblr Blog", posts: posts)
     send_resp(conn, 200, content)
   end
+
+
+  defp render(path, bindings)  do
+    full_path = Application.app_dir(:dumblr, "priv/web/" <> path)
+    content = EEx.eval_file(full_path, bindings)
+  end
+
 
   def start do
     Logger.info("Dumblr starting on port 8000")
